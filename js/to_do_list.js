38a24de2d6a238fa302b58a5818f0696e6b8e312
Yu <yuzyuan93@gmail.com>
2019-10-09 22:14:16 -0400

    Added functionality to buttons

--- js/to_do_list.js
+++ js/to_do_list.js
@@ -2,37 +2,38 @@
 $(document).ready(function() {
     var dt = new Date();
     document.querySelector('#date').innerHTML = dt.toLocaleDateString();
-    console.log("is this ready?");
+    //console.log("is this ready?");
 
 var taskFd = $('#newTaskField');
 var taskLabel = $('#newTaskLabel');
 var addBtn = $('#addItemBtn');
+var btnDiv = document.getElementById('btn-styles');
 
 
 taskFd.on('focus', function(){
     taskFd.css('padding', '10px');
-    console.log("width: " + taskFd.width());
+    //console.log("width: " + taskFd.width());
 }) 
 
 taskFd.on('keyup', function() {
-  console.log("focused");
+  //console.log("focused");
   //when user inputs into field
   //show the add button
   //taskFd.css('padding', '10px');
   if(!taskFd.val()) {
     addBtn.hide();
-    console.log("width: " + taskFd.width());
-    console.log("add is hidden");
+    //console.log("width: " + taskFd.width());
+    //console.log("add is hidden");
   }
   else {
     addBtn.show();
-    console.log("width: " + taskFd.width());
-    console.log("add is showing");
+    //console.log("width: " + taskFd.width());
+    //console.log("add is showing");
   }
 })
 
 taskFd.on('blur', function() {
-  console.log("blurred");
+  //console.log("blurred");
   //when user clears out/empty task
   //show taskLabel
   //hide the add button
@@ -50,27 +51,76 @@ taskFd.on('blur', function() {
 })
 
 addBtn.on('click', function(){
-  console.log("add new item");
+  //console.log("add new item");
   var li = document.createElement("li");
   var ul = document.getElementById("myUL");
   var inputVal = taskFd.val();
-  console.log(inputVal);
-  var t = document.createTextNode(inputVal);
+  //console.log(inputVal);
+  var text = document.createTextNode(inputVal);
+  var span = document.createElement("SPAN");
   var checkBtn = document.createElement("BUTTON");
   var deleteBtn = document.createElement("BUTTON");
+  var editBtn = document.createElement("BUTTON");
   var checkText = document.createTextNode("Check");
   var deleteText = document.createTextNode("X");
+  var editText = document.createTextNode("Edit");
+  var newInput = document.createElement('INPUT');
+  var inputText = document.createTextNode("placeholder");
+  newInput.className = "newInput";
+  span.className = "taskItem";
   checkBtn.className = "checkBtn";
   deleteBtn.className = "deleteBtn";
+  editBtn.className = "editBtn";
+  span.appendChild(newInput);
+  newInput.appendChild(inputText);
+  //newInput.hide();
+  span.appendChild(text);
   checkBtn.appendChild(checkText);
   deleteBtn.appendChild(deleteText);
-  console.log("t: " + t);
-  li.appendChild(t);
-  li.appendChild(checkBtn);
-  li.appendChild(deleteBtn);
+  editBtn.appendChild(editText);
+  li.appendChild(span);
   ul.appendChild(li);
+  li.appendChild(deleteBtn);
+  li.appendChild(editBtn);
+  li.appendChild(checkBtn);
   //clear out input
   taskFd.val('');
+  
+  //deleteBtn.onclick = removeItem(li);
 })
 
 })
+
+//var deleteBtn = $('.deleteBtn');
+
+$(document).on("click", '.deleteBtn', function() {
+    $(this).closest("li").fadeOut(function() {
+      $(this).remove();
+    });
+    return false;
+})
+
+$(document).on("click", '.checkBtn', function() {
+    //console.log("check button clicked");
+    $(this).closest("li").find("span").toggleClass('striked');
+});
+
+$(document).on("click", '.editBtn', function() {
+    if ($(this).closest("li").find("span").is(':visible')) {
+        console.log("inside if");
+        var val = $(this).closest("li").find("span").html();
+        $(this).closest("li").find("span").hide();
+        $(this).closest("li").find("input").show();
+        console.log("span value: " + val);
+        $(this).closest("li").find("input").val(val);
+        console.log("input value: " + $(this).closest("li").find("input").val(val));
+        $(this).html("Save");
+      } else {
+        console.log("else");
+        var val = $(this).closest("li").find("#newInput").val();
+        $(this).closest("li").find("#newInput").hide();
+        $(this).closest("li").find("span").show();
+        $(this).closest("li").find("span").html(val);
+        $(this).html("Edit");
+      }
+});
